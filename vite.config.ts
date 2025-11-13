5017b9c731a8f4a5756918cddfc3ad175e2b4afb
Daniel Parks <dp+git@oxidized.org>
2025-11-12 21:32:58 -0800

    Updates from tic-tac-toe example.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    checker({
+      typescript: {
+        buildMode: true,
+      },
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "src/**/*.{ts,tsx}"',
+      },
+    }),
+    react(),
+  ],
+});
