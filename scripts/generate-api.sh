0d36efb7c4a6ae25a87b57c020a73d6d92f3c4dd
Daniel Parks <dp+git@oxidized.org>
2025-12-22 11:25:45 -0800

    Move `openapi.json` into `src/api/`.

--- scripts/generate-api.sh
+++ scripts/generate-api.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-deno run -A npm:@oxide/openapi-gen-ts@0.12.0 openapi.json src/api
+deno run -A npm:@oxide/openapi-gen-ts@0.12.0 src/api/openapi.json src/api
 
 # Sloppy imports aren't supported, so we need to rewrite them. sed in-place
 # isn't portable without creating backup files, and PCRE is nicer anyway:
