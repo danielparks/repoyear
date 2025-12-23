a0b14f3c908e8b1f2bbb3b993f6000f20893d834
Daniel Parks <dp+git@oxidized.org>
2025-12-22 23:47:07 -0800

    `get-version.sh`: Suppress output from `git update-index`.

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -2,6 +2,6 @@
 
 # Make sure the index matches what’s on disk. Without this the version might
 # contain "-dirty" even if there are no apparent changes in `git status``.
-git update-index --refresh -q || true
+git update-index --refresh -q &>/dev/null || true
 
 git describe --dirty --broken --always --match 'v*'
