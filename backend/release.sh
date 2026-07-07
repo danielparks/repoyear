ba386ac6df82a5b552e646857249434664a3efa2
Daniel Parks <dp+git@oxidized.org>
2026-07-07 00:50:52 -0700

    `release.sh`: tweak comment.

--- backend/release.sh
+++ backend/release.sh
@@ -35,7 +35,7 @@ crate-names () {
 }
 
 crate-names-to-publish () {
-  # I’m not sure why publish shows up as [] for false and null for true.
+  # I’m not sure why publish is [] for false and null for true (the default).
   cargo metadata --format-version 1 --no-deps \
     | jq -r '.packages[] | select(.publish == null) |.name'
 }
